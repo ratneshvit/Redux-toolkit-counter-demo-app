8e0e7d6764d01d86596d54fab576b74926359ea7
Ratnesh Shukla <shuklav71@gmail.com>
2023-01-04 18:20:58 +0530

    react js with redux toolkit demo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      {/* {"aaa"} */}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
